ce10e71f7e822c4942edca5bcef114fdbc7be9a2
dabdevelop <develop@dab.vc>
2018-05-23 19:01:50 +0800

    Changes

--- js/nebConfig.js
+++ js/nebConfig.js
@@ -1,3 +1,4 @@
+var faucet = "2f9ab0f3a013f35313453125d60b42286eba22e72a907ab1afd670163efd042e";
 var dappAddress = "n233EsTRKjH4vCgrR9Yo1opnZSvVRA5f2Xs";
 var chainId = 1;
 var rpcURL = "https://mainnet.nebulas.io";
