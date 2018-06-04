281a783504530f1b01a1a92a5c9e6cc41cd76637
dabdevelop <develop@dab.vc>
2018-06-05 00:13:38 +0800

    Changes

--- js/airdropConfig.js
+++ js/airdropConfig.js
@@ -1,6 +1,6 @@
-var promotionId = 12;
+var promotionId = 16;
 var goldMasterPrize = [7, 13];
 var goldMasterPrizeIndex = 1;
-var promotion = [2, 12];
+var promotion = [16, 12];
 var airdrop = [];
-var hide = [0, 4, 9, 6, 1, 11, 10, 14];
+var hide = [0, 4, 9, 6, 1, 11, 10, 14, 2];
